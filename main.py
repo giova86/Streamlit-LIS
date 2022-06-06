0f5f1f3d21de8790e5bcfd0cca0680d9964e3889
Giovanni Bocchi <giovanni.bocchi@kiwidatascience.com>
2022-06-06 11:13:34 +0200

    first push

---
+++ main.py
@@ -0,0 +1,153 @@
+from streamlit_webrtc import webrtc_streamer
+import av
+import cv2
+from utils import mediapipe_detection, draw_landmarks, draw_landmarks_custom, draw_limit_rh, draw_limit_lh, check_detection, points_detection
+import pickle
+from sklearn import svm
+import numpy as np
+import mediapipe as mp
+import time
+import streamlit as st
+import requests
+from streamlit_lottie import st_lottie
+
+lotties_coding = "https://assets7.lottiefiles.com/packages/lf20_twhevzbe.json"
+
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+st.set_page_config(layout='wide')
+
+
+def local_CSS(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+local_CSS('style.css')
+
+# load svm model
+model = pickle.load(open('models/model_svm_all.sav', 'rb'))
+labels = np.array(model.classes_) # put the entire alphabet in the future
+
+mp_holistic = mp.solutions.holistic
+mp_drawing = mp.solutions.drawing_utils
+words = []
+
+class VideoProcessor:
+    def recv(self, frame):
+        frame = frame.to_ndarray(format="bgr24")
+
+        with mp_holistic.Holistic(min_detection_confidence=0.5,
+                                  min_tracking_confidence=0.5) as holistic:
+
+            h, w, c = frame.shape
+
+            # make detection
+            image, results = mediapipe_detection(frame, holistic)
+
+            color = (0,0,0)
+            #cv2.rectangle(frame, (0+int(0.03*h),int(h-0.14*h)), (0+int(0.75*h), int(h-0.015*h)), color,-1)
+            cv2.rectangle(frame, (0, 0),
+                                 (int(w*0.18), int(h-h*0.12)), (255,255,255),-1)
+
+
+            for i in range(len(labels)):
+    #            cv2.rectangle(frame, (90, 10+ i*int(50)), (90, 60+ i*int(50)), color,-1)
+                cv2.putText(frame, labels[i], (50, (i+1)*int(h/(len(labels)+4))), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,0), 2, cv2.LINE_AA)
+                cv2.rectangle(frame, (90, (i)*int(h/(len(labels)+4))+30),
+                                     (90, (i+1)*int(h/(len(labels)+4)) ), color,-1)
+
+            # perform prediction with relative probability
+            if results.right_hand_landmarks:
+
+                # draw_limit_rh(frame, results)
+
+                # uncomment for NN
+                # prediction = labels[np.argmax(model.predict(np.array([points_detection(results)])))]
+
+                prediction = model.predict(np.array([points_detection(results)]))[0]
+                pred_prob = np.max(model.predict_proba(np.array([points_detection(results)])))
+
+                for i in range(len(labels)):
+    #                cv2.rectangle(frame, (70, 10+ i*int(50)), (70+int(model.predict_proba(np.array([points_detection(results)]))[0][i]*100)*3, 60+ i*int(50)), color,-1)
+                    cv2.putText(frame, labels[i], (50, (i+1)*int(h/(len(labels)+4))), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,0), 2, cv2.LINE_AA)
+                    cv2.rectangle(frame, (90, (i)*int(h/(len(labels)+4))+30),
+                                         (90+int(model.predict_proba(np.array([points_detection(results)]))[0][i]*60)*2, (i+1)*int(h/(len(labels)+4)) ), color,-1)
+
+                # uncomment for NN
+                # for i in range(len(labels)):
+                #     cv2.rectangle(frame, (70, 10+ i*int(50)), (70+int(model.predict(np.array([points_detection(results)]))[0][i]*100)*3, 60+ i*int(50)), color,-1)
+                #     cv2.putText(frame, labels[i], (10, 50+ i*int(50)), cv2.FONT_HERSHEY_SIMPLEX, 2,(0,255,0), 4, cv2.LINE_AA)
+
+
+                # add text with prediction
+                if pred_prob > 0.5:
+                    cv2.putText(frame, f'{prediction.capitalize()} ({int(pred_prob*100)}%)',
+                                (0+int(0.05*h),h-int(0.05*h)),
+                                cv2.FONT_HERSHEY_SIMPLEX,
+                                2 ,
+                                (0,255,0),
+                                2,
+                                cv2.LINE_AA)
+                elif pred_prob < 0.3:
+                    cv2.putText(frame, 'I am not sure...',
+                                (0+int(0.05*h),h-int(0.05*h)),
+                                cv2.FONT_HERSHEY_SIMPLEX,
+                                2 ,
+                                (0, 0, 255),
+                                2,
+                                cv2.LINE_AA)
+                else:
+                    cv2.putText(frame, f'Maybe {prediction.capitalize()} ({int(pred_prob*100)}%)',
+                                (0+int(0.05*h),h-int(0.05*h)),
+                                cv2.FONT_HERSHEY_SIMPLEX,
+                                2 ,
+                                (45, 255, 255),
+                                2,
+                                cv2.LINE_AA)
+            else:
+                cv2.putText(frame, 'Detecting Hand...',
+                            (w-int(0.5*h),int(0.05*h)),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            2,
+                            (0,0,0),
+                            2,
+                            cv2.LINE_AA)
+
+
+
+
+            return av.VideoFrame.from_ndarray(frame, format='bgr24')
+
+
+st.subheader('This is my Page')
+st.title('LIS Alphabet')
+st.write("Italian Sign Language or LIS (Lingua dei Segni Italiana) is the visual language used by deaf people in Italy. Deep analysis of it began in the 1980s, along the lines of William Stokoe's research on American Sign Language in the 1960s. Until the beginning of the 21st century, most studies of Italian Sign Language dealt with its phonology and vocabulary. According to the European Union for the Deaf, the majority of the 60,000–90,000 Deaf people in Italy use LIS [Wikipedia].")
+webrtc_streamer(key='key', video_processor_factory=VideoProcessor,
+    media_stream_constraints={
+        "video": {
+            "width": 1280,
+            "frameRate": 30
+        }
+    },)
+
+
+
+st.write("---")
+
+left_column, right_column = st.columns(2)
+with right_column:
+        st.header("GitHub Project")
+        st.write("##")
+        st.write(
+        """
+        On my GitHub channel you can find the repository of the LIS project:
+        - djskalò djasdjk sa
+        - jdkaslò jdsaòlkjd asòkjds aòklds
+        - hda slhkjd sahldsa hdsalkj
+        """
+        )
+with left_column:
+    st_lottie(lotties_coding, height=300, key='coding')
